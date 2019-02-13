7aa89c267a8cbd0fe784977ba80e85321d534bf2
evborisova <46461478+evborisova@users.noreply.github.com>
2019-02-13 21:24:50 +0100

    Initial work

---
+++ src/WebApplication1/Controllers/LudoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace WebApplication1.Controllers
+{
+    public class LudoController : Controller
+    {
+        public IActionResult Index()
+        {
+            var client = new RestClient("http://localhost:56104");
+
+            var request = new RestRequest("api/Ludo", Method.GET);
+            
+            var ludoGameResponse = client.Execute<List<int>>(request);
+            var gameName = ludoGameResponse.Data;
+
+            return View();
+        }
+    }
+
+}
